e6b2da50caec8aaf99b285f3277e0180aa2364e4
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-15 11:56:41 -0300

    step 4/23

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,9 +3,7 @@ from django.templatetags.static import static
 
 import json
 from django.http import HttpResponseBadRequest
-
-
-from .models import Product
+from .models import Product, Order, OrderItem
 
 
 def banners_list_api(request):
@@ -71,14 +69,26 @@ def product_list_api(request):
 
 
 def register_order(request):
-    # TODO это лишь заглушка
-
     if request.method == "POST":
         try:
             data = json.loads(request.body)
-            # Обработка данных в формате JSON
-            # ...
-            print(data)
+
+            new_order = Order(
+                first_name=data["firstname"],
+                last_name=data["lastname"],
+                phone_number=data["phonenumber"],
+                address=data["address"],
+            )
+            new_order.save()
+
+            for p in data["products"]:
+                new_item = OrderItem(
+                    order=new_order,
+                    product=Product.objects.get(id=p["product"]),
+                    quantity=p["quantity"],
+                )
+                new_item.save()
+
             return JsonResponse(data)
         except json.JSONDecodeError:
             return HttpResponseBadRequest("Invalid JSON")
