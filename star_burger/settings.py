e97cce7b69573a711550d1472a0beb2348cafd2d
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-05-30 07:44:25 -0300

    allowed host added

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = env("SECRET_KEY")
 YANDEX_GEOCODER_API_KEY = env("YANDEX_GEOCODER_API_KEY")
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["127.0.0.1", "localhost", "77.105.168.57"])
 
 INSTALLED_APPS = [
     "foodcartapp.apps.FoodcartappConfig",
